a971dffc8197db4f2631eac8dcd6fea38b8f93e6
JubairAhmad <jubairahmadrumman@gmail.com>
2021-10-05 04:56:01 +0600

    changed

--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -5,7 +5,7 @@ const NotFound = () => {
         <div className='container text-center m-28'>
             <div>
            <h1 className='text-9xl'>404 '-_-"</h1>
-           <h1 className='text-9xl'>Page not found</h1>
+           <h1 className='text-9xl  '>Page not found</h1>
             </div>
         </div>
     );
