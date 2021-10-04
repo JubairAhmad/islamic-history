b91a68bb5da085c0a17bdca697210f6a14567f32
JubairAhmad <jubairahmadrumman@gmail.com>
2021-10-05 04:41:13 +0600

    design chaned

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='bg-gray-300 text-black pt-20 pb-20'>
-            <div className='container text-center font-bold'>
+            <div className='container text-center font-bold text-4xl'>
                 <small>Copyright © 2021 Learn Islamic History</small>
             </div>
         </div>
--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -4,8 +4,8 @@ const NotFound = () => {
     return (
         <div className='container text-center m-28'>
             <div>
-           <h1>404 '-_-"</h1>
-           <h1>Page not found</h1>
+           <h1 className='text-9xl'>404 '-_-"</h1>
+           <h1 className='text-9xl'>Page not found</h1>
             </div>
         </div>
     );
